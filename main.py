718a198d33f99fde4ff7b2ed2e777155acd627c5
blip <blip31.69@gmail.com>
2023-10-03 01:48:43 -0400

    Description of changes

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-##main.py
+from selenium import webdriver
+import time
 
-print("hello world")
+def main():
+    # Initialize the WebDriver and specify the path to chromedriver
+    driver = webdriver.Chrome(executable_path='/path/to/chromedriver')
+
+    # Navigate to the URL
+    driver.get('https://kick.com/internetentourage')
+
+    # Optional: Simulate some interactions
+    # For example, to click a button with the ID 'playButton':
+    # driver.find_element_by_id('playButton').click()
+
+    # Wait for a few seconds
+    time.sleep(5)
+
+    # Close the WebDriver
+    driver.quit()
+
+if __name__ == "__main__":
+    main()
